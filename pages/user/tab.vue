044467abc4743a409bc46c88a26d815123429d88
wanyufang <13922701577@163.com>
2019-09-01 15:47:29 +0800

    创建项目

---
+++ pages/user/tab.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>
+      <span
+        v-for="(item,index) in ['新闻','体育','娱乐']"
+        :key="index"
+        @click="clickCut(index)"
+        :class="{red: create === index }"
+      >{{item}}</span>
+    </div>
+    <div v-show="create === 0">
+      <ul>
+        <li>新闻11111</li>
+        <li>新闻11111</li>
+        <li>新闻11111</li>
+      </ul>
+    </div>
+    <div v-show="create === 1">
+      <ul>
+        <li>体育2222222</li>
+        <li>体育2222222</li>
+        <li>体育2222222</li>
+      </ul>
+    </div>
+    <div v-show="create === 2">
+      <ul>
+        <li>娱乐33333</li>
+        <li>娱乐33333</li>
+        <li>娱乐33333</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        create: 0
+    };
+  },
+  methods: {
+      clickCut(index){
+          this.create = index
+      }
+  }
+};
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
